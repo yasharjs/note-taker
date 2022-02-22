1529baaf2effd552eb600fa438cf3a4b636e2d9f
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2022-02-21 22:07:55 -0500

    modularized the files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// parse incoming string or array
+app.use(express.urlencoded({extended: true}));
+// parse incoming json data
+app.use(express.json());
+// access to the public files
+app.use(express.static('public'));
+// connect the routes 
+app.use('/api',apiRoutes);
+app.use('/',htmlRoutes);
+
+
+app.listen(PORT, () =>{
+    console.log("API server now on port ",PORT);
+})
